9fcd2a8f5e7037a8f1a99621e7588251532f50f7
KLVN <KLVN@users.noreply.github.com>
2014-09-18 18:14:07 +0200

    Improved Strcture + Removed JQuery
    Moved JS to my-app.js // Now without using JQuery

--- js/my-app.js
+++ js/my-app.js
@@ -1,14 +1,42 @@
 var myApp = new Framework7({
-    modalTitle: 'UrbanDic API Test',
-    modalPreloaderTitle: 'Loading',
-    modalButtonCancel: 'Cancel',
-    pushState: false,
-    onAjaxStart: function (xhr) { myApp.showPreloader();},
-    onAjaxComplete: function (xhr) { myApp.hidePreloader();},
-    sortable: false,
-    swipeout: false,
-	cacheDuration: 1000*60*3,
 });
+
+var $$ = Dom7;
+
+// Select Template
+var template = document.getElementById('random-template').innerHTML;
+
+// Compile and render
+var compiledTemplate = Template7.compile(template);
+
+// Defined as function "getrandom"
+function getrandom() {
+	// Get JSON Data from UrbanDictionary API 
+	$$.getJSON ('http://api.urbandictionary.com/v0/random', function (json) {
+
+	// Insert rendered template
+	document.getElementById('content-wrap').innerHTML = compiledTemplate(json);
+	});
+};
+
+// Execute to list UrbanDictionary Definitions
+getrandom();
+
+// Select Pull to refresh content
+var ptrContent = $$('.pull-to-refresh-content');
+
+// On refresh
+ptrContent.on('refresh', function (e) {
+	// Emulate 1s loading
+	setTimeout(function () {
+
+		// Execute getrandom to get new Definitions
+		getrandom();
+
+	myApp.pullToRefreshDone();
+	}, 1000);
+});
+
 
 var mainView = myApp.addView('.view-main', {
     dynamicNavbar: true
